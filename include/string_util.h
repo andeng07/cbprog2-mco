a5fd70cc230e62340e27906452216833425e3667
Andrei Sumilang <sumilangandreijohn@gmail.com>
2026-02-20 13:41:02 +0800

    feat(strings): add String8/32/64 types and str_equal/str_split functions

---
+++ include/string_util.h
@@ -0,0 +1,12 @@
+#ifndef STRING_UTIL
+#define STRING_UTIL
+
+typedef char String8[9];
+typedef char String32[33];
+typedef char String64[65];
+
+int str_equal(const char a[], const char b[]);
+
+int str_split(char (*dest)[], const char src[], const char delimeter);
+
+#endif
